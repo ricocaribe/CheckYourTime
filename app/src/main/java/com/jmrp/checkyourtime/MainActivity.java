882be6d585ad8cda63a45a418c330c18055cc0b8
Jose Maria Rico <jmricop@gmail.com>
2017-08-04 13:23:30 +0200

    - Layout ready a falta de retoques

--- app/src/main/java/com/jmrp/checkyourtime/MainActivity.java
+++ app/src/main/java/com/jmrp/checkyourtime/MainActivity.java
@@ -6,7 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.AppCompatSpinner;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -16,6 +16,7 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Calendar;
@@ -27,9 +28,12 @@ public class MainActivity extends AppCompatActivity {
     private EditText userBirthdate;
     private Calendar myCalendar = Calendar.getInstance();
     private Spinner spinner_units;
+    private EditText edt_units;
     private Spinner spinner_time_interval;
-    private Spinner spinner_duration;
-
+    private Spinner spinner_ocurrences;
+    private EditText edt_ocurrences;
+    private boolean enabledFirst = true;
+    private boolean enabledSecond = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void configSpinnerTime(){
+        edt_units = (EditText) findViewById(R.id.edt_units);
+
         spinner_units = (Spinner) findViewById(R.id.spinner_units);
 
         // Initializing an ArrayAdapter
@@ -69,19 +75,38 @@ public class MainActivity extends AppCompatActivity {
 
         adapterTime.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner_units.setAdapter(adapterTime);
+
+        spinner_units.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                // your code here
+                if(position != 0) spinner_time_interval.setEnabled(true);
+                else {
+                    spinner_time_interval.setSelection(0);
+                    spinner_time_interval.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
     }
 
 
     private void configSpinnerTimeIntervals(){
         spinner_time_interval = (Spinner) findViewById(R.id.spinner_time_interval);
+        spinner_time_interval.setEnabled(false);
 
         // Initializing an ArrayAdapter
-        List<String> items = Arrays.asList(getResources().getStringArray(R.array.time_units));
+        final List<String> items = Arrays.asList(getResources().getStringArray(R.array.time_units));
         final ArrayAdapter<String> adapterTime = new ArrayAdapter<String>(
                 this, android.R.layout.simple_spinner_item, items){
             @Override
-            public boolean isEnabled(int position){
-                return position != 0;
+            public boolean isEnabled(int position) {
+                return position!=0;
             }
 
             @Override
@@ -95,11 +120,42 @@ public class MainActivity extends AppCompatActivity {
 
         adapterTime.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner_time_interval.setAdapter(adapterTime);
+
+        spinner_time_interval.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                // your code here
+                if(position == 1) {
+                    spinner_ocurrences.setEnabled(true);
+                    enabledFirst = false;
+                    enabledSecond = true;
+                }
+                else if(position == 2) {
+                    spinner_ocurrences.setEnabled(true);
+                    enabledFirst = false;
+                    enabledSecond = false;
+                }
+                else if(position==3) {
+                    spinner_ocurrences.setEnabled(true);
+                    enabledFirst = false;
+                    enabledSecond = false;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
     }
 
 
     private void configSpinnerTimeUnits(){
-        spinner_duration = (Spinner) findViewById(R.id.spinner_time_units);
+        edt_ocurrences = (EditText) findViewById(R.id.edt_occurrences);
+
+        spinner_ocurrences = (Spinner) findViewById(R.id.spinner_ocurrences);
+        spinner_ocurrences.setEnabled(false);
 
         // Initializing an ArrayAdapter
         List<String> items = Arrays.asList(getResources().getStringArray(R.array.time_units));
@@ -107,7 +163,7 @@ public class MainActivity extends AppCompatActivity {
                 this, android.R.layout.simple_spinner_item, items){
             @Override
             public boolean isEnabled(int position){
-                return position != 0;
+                return position != 0 && !(!enabledFirst && position == 1) && !(!enabledSecond && position == 2);
             }
 
             @Override
@@ -115,12 +171,14 @@ public class MainActivity extends AppCompatActivity {
                 View view = super.getDropDownView(position, convertView, parent);
                 TextView tv = (TextView) view;
                 if(position == 0) tv.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.secondary_text));
+                else if(!enabledFirst && position==1) tv.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.secondary_text));
+                else if(!enabledSecond && position==2) tv.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.secondary_text));
                 return view;
             }
         };
 
         adapterTime.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner_duration.setAdapter(adapterTime);
+        spinner_ocurrences.setAdapter(adapterTime);
     }
 
 
@@ -154,13 +212,15 @@ public class MainActivity extends AppCompatActivity {
 
     public void calculate(View view){
         long totalLifeTime = System.currentTimeMillis() - myCalendar.getTimeInMillis();
-        long totalTime = getDedicatedTime()*getTimeInterval();
+        long totalTime = getDedicatedTime();
 
-        String time = String.valueOf(totalTime/totalLifeTime);
-        String percent = String.valueOf((totalTime*100)/totalLifeTime);
+        float percent = round((totalTime*100)/totalLifeTime, 2);
+
+        Log.i(getClass().getSimpleName(), "TotalTime: " + totalTime);
+        Log.i(getClass().getSimpleName(), "Percent: " + percent);
 
         Intent intent = new Intent(MainActivity.this, ResultActivity.class);
-        intent.putExtra("TIME", time);
+        intent.putExtra("TIME", totalTime);
         intent.putExtra("PERCENT", percent);
         startActivity(intent);
 
@@ -170,21 +230,30 @@ public class MainActivity extends AppCompatActivity {
     private long getDedicatedTime(){
         long dedicatedTime = 0L;
 
-        if(spinner_units.getSelectedItemPosition()==0) dedicatedTime = Long.parseLong(spinner_units.getSelectedItem().toString())*1000;
-        else if(spinner_units.getSelectedItemPosition()==1) dedicatedTime = Long.parseLong(spinner_units.getSelectedItem().toString())*60000;
-        else if(spinner_units.getSelectedItemPosition()==2) dedicatedTime = Long.parseLong(spinner_units.getSelectedItem().toString())*3600000;
+        if(spinner_units.getSelectedItemPosition()==1) dedicatedTime = (long)(Integer.parseInt(edt_units.getText().toString())*1000);
+        else if(spinner_units.getSelectedItemPosition()==2) dedicatedTime = (long)(Integer.parseInt(edt_units.getText().toString())*60000);
+        else if(spinner_units.getSelectedItemPosition()==3) dedicatedTime = (long)(Integer.parseInt(edt_units.getText().toString())*3600000);
 
+
+        if(spinner_time_interval.getSelectedItemPosition()==1 && spinner_ocurrences.getSelectedItemPosition()==2) {
+            dedicatedTime = Long.parseLong(edt_ocurrences.getText().toString())*30*dedicatedTime;
+        }
+        else if(spinner_time_interval.getSelectedItemPosition()==1 && spinner_ocurrences.getSelectedItemPosition()==3) {
+            dedicatedTime = Long.parseLong(edt_ocurrences.getText().toString())*365*dedicatedTime;
+        }
+
+        if(spinner_time_interval.getSelectedItemPosition()==2 && spinner_ocurrences.getSelectedItemPosition()==3) {
+            dedicatedTime = Long.parseLong(edt_ocurrences.getText().toString())*12*dedicatedTime;
+        }
+
+        Log.i(getClass().getSimpleName(), "DedicatedTime: " + dedicatedTime);
         return dedicatedTime;
     }
 
-    private int getTimeInterval(){
-        int interval = 0;
 
-        if(spinner_duration.getSelectedItemPosition()==0) return 1;
-        else if(spinner_duration.getSelectedItemPosition()==1) return 30;
-        else if(spinner_duration.getSelectedItemPosition()==2) return 365;
-
-        return interval;
+    public static float round(float d, int decimalPlace) {
+        BigDecimal bd = new BigDecimal(Float.toString(d));
+        bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
+        return bd.floatValue();
     }
-
 }
--- app/src/main/java/com/jmrp/checkyourtime/ResultActivity.java
+++ app/src/main/java/com/jmrp/checkyourtime/ResultActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.concurrent.TimeUnit;
+
 public class ResultActivity extends AppCompatActivity {
 
     @Override
@@ -12,9 +14,23 @@ public class ResultActivity extends AppCompatActivity {
         setContentView(R.layout.activity_result);
 
         TextView txtTime = (TextView) findViewById(R.id.txt_result_time);
-        txtTime.setText(getIntent().getStringExtra("TIME") + "miliseconds");
-        TextView txtPercent = (TextView) findViewById(R.id.txt_result_percent);
-        txtPercent.setText(getIntent().getStringExtra("PERCENT") + "%");
+        long millis =  getIntent().getLongExtra("TIME", 0L);
+        String hms = String.format("%02dhh:%02dmm:%02dss", TimeUnit.MILLISECONDS.toHours(millis),
+                TimeUnit.MILLISECONDS.toMinutes(millis) % TimeUnit.HOURS.toMinutes(1),
+                TimeUnit.MILLISECONDS.toSeconds(millis) % TimeUnit.MINUTES.toSeconds(1));
+        txtTime.setText(hms);
 
+        TextView txtPercent = (TextView) findViewById(R.id.txt_result_percent);
+        txtPercent.setText(getIntent().getFloatExtra("PERCENT", 0.0f) + "% de tu vida XD");
+
+    }
+
+    private String secondsToString(long seconds) {
+        double numyears = Math.floor(seconds / 31536000);
+        double numdays = Math.floor((seconds % 31536000) / 86400);
+        double numhours = Math.floor(((seconds % 31536000) % 86400) / 3600);
+        double numminutes = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60);
+        double numseconds = (((seconds % 31536000) % 86400) % 3600) % 60;
+        return numyears + " years " +  numdays + " days " + numhours + " hours " + numminutes + " minutes " + numseconds + " seconds";
     }
 }
